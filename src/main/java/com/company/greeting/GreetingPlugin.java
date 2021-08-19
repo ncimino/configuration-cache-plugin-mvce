37409b3a979828e79464a50c2d4f7c784aafa13b
Nik <nik.cimino@gmail.com>
2021-08-19 17:39:07 -0600

    Removes dead code in GreetingPlugin

--- src/main/java/com/company/greeting/GreetingPlugin.java
+++ src/main/java/com/company/greeting/GreetingPlugin.java
@@ -8,10 +8,5 @@ class GreetingPlugin implements Plugin<Project> {
     public void apply(Project project) {
         GreetingPluginExtension extension = project.getExtensions().create("greeting", GreetingPluginExtension.class);
         HelloTask hello = project.getTasks().create("hello", HelloTask.class);
-/*
-        hello.doLast({
-            println("${extension.message.get()} from ${extension.greeter.get()}");
-        });
-*/
     }
 }
